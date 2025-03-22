2fe4306ff148e23fd7bfc12cf20707287f1baa21
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-03-22 08:30:49 +0530

    hii

---
+++ 06_loops.js
@@ -0,0 +1,18 @@
+// for loop
+for (initialization; condition; increment / decrement) {
+    // Code to execute
+}
+
+
+// while loop
+while (condition) {
+    // Code to execute
+}
+
+
+// do while loop
+
+
+do {
+    // Code to execute
+} while (condition);
---
+++ exp_loop.js
@@ -0,0 +1,25 @@
+//javascript program to illustrate for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration:", i);
+}
+
+
+
+
+//javascript program to illustrate while loop
+
+let j = 0;
+while (j < 5) {
+    console.log("Iteration:", j);
+    j++;
+}
+
+
+//javascript program to illustrate do - while loop
+
+
+let i = 0;
+do {
+    console.log("Iteration:", i);
+    i++;
+} while (i < 5);
