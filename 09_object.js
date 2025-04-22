42343e5e3f806c4a216a13a1ed9ed1171891f295
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-04-23 00:07:54 +0530

    same

--- 09_object.js
+++ 09_object.js
@@ -1,13 +1,13 @@
-let obj = {
-    name: "john doe",
-    age: 30,
-    city: "new york",
-    country: "usa",
-    "latest city": "los angeles",
-}
-console.log(obj);
-console.log(obj.country);
-console.log(obj["latest city"]);
+// let obj = {git
+//     name: "john doe",
+//     age: 30,
+//     city: "new york",
+//     country: "usa",
+//     "latest city": "los angeles",
+// }
+// console.log(obj);
+// console.log(obj.country);
+// console.log(obj["latest city"]);
 
 //Function in Object
 let obj = {
