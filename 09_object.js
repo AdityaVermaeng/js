574cf8ad5edff9212c669016e79fcbe03b40d470
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-04-23 00:06:52 +0530

    Object in js

---
+++ 09_object.js
@@ -0,0 +1,19 @@
+let obj = {
+    name: "john doe",
+    age: 30,
+    city: "new york",
+    country: "usa",
+    "latest city": "los angeles",
+}
+console.log(obj);
+console.log(obj.country);
+console.log(obj["latest city"]);
+
+//Function in Object
+let obj = {
+    fun: function() {
+        console.log("hello");
+    }
+};
+
+obj.fun(); // Outputs: hello
