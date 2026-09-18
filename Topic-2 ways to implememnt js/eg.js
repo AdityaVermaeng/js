812b03e9447949befb458ef4a87d2d3c99e7653f
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-09-18 12:16:20 +0530

    feat:add ways of implementaion of js

---
+++ Topic-2 ways to implememnt js/eg.js
@@ -0,0 +1,23 @@
+// let  var const difference
+
+//for var
+
+var a=10;
+var a=100;
+var a=1000;
+a=10000;
+console.log(a) //output=10000;
+
+//let
+ let b=20;
+//  let b=200;//not write like this cause it follow one time declaration
+b=200;
+b=50;
+console.log(b);
+
+//const
+
+const c= 30;
+// const c= 40//
+// c=300;//TypeError: Assignment to constant variable.
+console.log(c);
