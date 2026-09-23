d226e0391f00502c7a5bab38caf8ee5fe9aa6f32
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-09-23 18:22:11 +0530

    feat: add Global Execution Context

---
+++ Topic-5 Scope/gEC.js
@@ -0,0 +1,21 @@
+//Global Execution context
+
+console.log("Start");
+var a=10;
+let b=20;
+const c=30;
+{
+var a=100;
+let b=200;
+const c=300;
+var d=400;
+console.log("inside the block");
+console.log("a",a);
+console.log("b",b);
+console.log("c",c);
+console.log("d",d);
+}
+console.log("a",a);
+console.log("b",b);
+console.log("c",c);
+console.log("End")
