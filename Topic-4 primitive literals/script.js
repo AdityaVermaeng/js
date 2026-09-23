acb1491013490c46f9006e2a6b581762b6c3a036
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-09-23 18:14:45 +0530

    feat: added primitive literals

---
+++ Topic-4 primitive literals/script.js
@@ -0,0 +1,17 @@
+// let a;
+// console.log(a);
+// console.log(b);
+
+//string
+let str ='hello';
+console.log(str)
+console.log(str[2]);
+// console.log(cell)
+
+//template literal
+
+let user ="kundan";
+let meme=`${user}
+are bhai dj banao apna q pareshan ho rhe ho`;
+console.log(meme);
+// console.log(meme[1])
